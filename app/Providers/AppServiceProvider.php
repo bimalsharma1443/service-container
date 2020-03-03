eea7ff9a992f81f84850c2b0f8e5ed842c99a2c4
Bimal Sharma <Administrator@SSLBCGWC0R16155.SBICAPSECU.COM>
2020-03-03 21:21:54 +0530

    service container check in laravel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Payment\PaymentDetail;
+use Faker\Provider\ar_SA\Payment;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        /*$this->app->bind(PaymentDetail::class,function(){
+            return new PaymentDetail('usd');
+        });*/
+
+        $this->app->singleton(PaymentDetail::class,function(){
+            return new PaymentDetail('usd');
+        });
     }
 
     /**
